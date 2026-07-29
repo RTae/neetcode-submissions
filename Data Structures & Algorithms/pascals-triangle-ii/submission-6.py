2d47a6c604561ad6702c8c78c410532355cf50ac
Natthanan Bhukan <natthanan.bhu@gmail.com>
2026-07-29 13:06:27 +0800

    Add: pascals-triangle-ii - submission-6

---
+++ Data Structures & Algorithms/pascals-triangle-ii/submission-6.py
@@ -0,0 +1,7 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        dp = [[1]*(i+1) for i in range(rowIndex+1)]
+        for i in range(rowIndex+1):
+            for j in range(1,i):
+                dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+        return dp[rowIndex]
