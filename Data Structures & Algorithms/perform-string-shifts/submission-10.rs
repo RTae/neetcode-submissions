752df0b3d676e65773162aa1dd5ab07bd8ae8068
Natthanan Bhukan <natthanan.bhu@gmail.com>
2026-08-26 12:35:31 +0700

    Add: perform-string-shifts - submission-10

---
+++ Data Structures & Algorithms/perform-string-shifts/submission-10.rs
@@ -0,0 +1,23 @@
+impl Solution {
+    pub fn string_shift(s: String, shift: Vec<Vec<i32>>) -> String {
+        // the idea is we need to make a sum of shift left or right into shift left amount
+        // if shift right it mean we remove the amount of shift left
+
+        // do a cal
+        let mut left_shift: i32=0;
+        for m in &shift {
+            if m[0] == 1 {
+                left_shift -= m[1];
+            } else {
+                left_shift += m[1];
+            }
+        }
+
+        // do a shift left
+        let n = s.len() as i32;
+        let left_shift = ((left_shift % n) + n) % n;
+        let mut bytes = s.into_bytes();
+        bytes.rotate_left(left_shift as usize);
+        return String::from_utf8(bytes).unwrap()
+    }
+}
