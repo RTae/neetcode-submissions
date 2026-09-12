bb87bccec74d8a9477cd47087afcd6eaf0206aa7
Natthanan Bhukan <natthanan.bhu@gmail.com>
2026-09-12 12:13:38 +0700

    Add: search-insert-position - submission-6

---
+++ Data Structures & Algorithms/search-insert-position/submission-6.py
@@ -0,0 +1,15 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        # Apply a binary search 
+        l, r = 0, len(nums)
+        while l < r:
+            # keep update center of array
+            m = l + ((r-l)//2)
+            # need to shift right
+            if nums[m] >= target:
+                r=m
+            # need to shift left
+            elif nums[m] < target:
+                l=m+1
+        # at cneter value
+        return l
